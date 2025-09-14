1ece8893e7b26352a5b236dec73756e7fdb03aaf
Corvin Dhali <corvindhali@icloud.com>
2025-09-14 20:44:33 +0100

    Create ultralight.f90

---
+++ Fortran/ultralight.f90
@@ -0,0 +1,9 @@
+program ultralight
+    implicit none
+    real :: m, c, E
+    ! Read mass from command-line argument
+    call getarg(1, m)
+    c = 299792458.0
+    E = m * c**2
+    print *, E
+end program ultralight
