2a5bae490674af497011a61d8ef6083f00d78ba0
Corvin Dhali <corvindhali@icloud.com>
2025-09-14 21:20:45 +0100

    Update ultralight.f90

--- Fortran/ultralight.f90
+++ Fortran/ultralight.f90
@@ -1,16 +1,10 @@
 program ultralight
     implicit none
     real :: m, c, E
-
-    ! Read mass from command-line argument
     call getarg(1, m)
-
-    ! Speed of light
     c = 299792458.0
-
-    ! Equation: E = mc^2
     E = m * c**2
-
     print *, E
 end program ultralight
+
 
